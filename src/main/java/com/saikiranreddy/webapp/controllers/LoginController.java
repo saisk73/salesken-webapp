0631489e7fccbb5744e877595e550426d2ee45ec
unknown <saisk73@gmail.com>
2021-06-17 22:43:10 +0530

    login functionality

--- src/main/java/com/saikiranreddy/webapp/controllers/LoginController.java
+++ src/main/java/com/saikiranreddy/webapp/controllers/LoginController.java
@@ -1,4 +1,4 @@
-package com.saikiranreddy.webapp;
+package com.saikiranreddy.webapp.controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 import com.saikiranreddy.webapp.model.User;
 import com.saikiranreddy.webapp.repository.UserRepository;
@@ -26,13 +28,24 @@ public class LoginController {
 	}
 	
 	@PostMapping(value = "/")
-	public String loginPost(Model model) {
-
-		return "redirect:/";
+	public String loginPost(HttpServletRequest request) {
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		System.out.println(email + " " + password);
+		User userData = userRepository.findOneByEmail(email);
+		if(userData == null) {
+			return "redirect:/";
+		}
+		String userPassword = userData.getPassword();
+		if(userPassword.equals(password)){
+			return "redirect:/dashboard";
+		} else {
+			return "redirect:/";
+		}
 	}
 
 	@GetMapping(value = "/login")
-	public String loginRedirect(Model model) {
+	public String loginRedirect(Model model) {	
 	    return "redirect:/";
 	}
 
--- src/main/java/com/saikiranreddy/webapp/repository/UserRepository.java
+++ src/main/java/com/saikiranreddy/webapp/repository/UserRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.saikiranreddy.webapp.model.User;
 public interface UserRepository extends CrudRepository<User, Integer> {
-    
+    User findOneByEmail(String email);
 }
